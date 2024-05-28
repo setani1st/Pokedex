7b2042faf2e4ab656a1eaff375fb8dd133c416d2
setani1st <setanijoao@gmail.com>
2024-05-27 23:38:18 -0300

    project finished,simple but the authentication works properly

--- pokedex/src/Components/Cards/PokemonCard.jsx
+++ pokedex/src/Components/Cards/PokemonCard.jsx
@@ -1,20 +1,24 @@
 import React from "react"
-import {Grid, Card, CardMedia, CardContent, Typography, makeStyles} from '@mui/material'
+import {Grid, Card, CardMedia, CardContent, Typography} from '@mui/material'
+//import makeStyles from '@mui/styled-engine-sc'
+import './PokemonCard.css'; 
 
-const useStyles = makeStyles((theme) => ({
+/*const useStyles = makeStyles((theme) => ({
     CardMedia: {
         margin: "auto",
-        
-    }
-}))
-const PokemonCard = (props) =>{
+        width:130,
+        height:130
 
+    }
+}))*/
+const PokemonCard = (props) =>{
+    /*const classes = useStyles();*/
     const {pokemon, image} = props
     const {id, name} = pokemon
     return(
         <Grid item xs={12} sm={2}>
-            <Card>
-                <CardMedia image={image}></CardMedia>
+            <Card className='Card'>
+                <CardMedia className='cardMedia' image={image}></CardMedia>
                 <CardContent>
                     <Typography>
                         {name}
